bf5cedb508539d7228e22b4e3ac558746a55dad4
Vladimir Grechukhin <inmosh@protomail.ch>
2021-04-14 01:19:25 +0300

    Change connect logic

--- bot/handlers/connect.py
+++ bot/handlers/connect.py
@@ -20,18 +20,12 @@ class Connect(BaseCommand):
     def _execute(self, user: dict, update: Update, context: CallbackContext) -> bool:
         if user['paused']:
             users.user_set_paused(user['chat_id'], False)
-        try:
-            exclude = chats.get_chat(user['chat_id'])['users']
-        except TypeError:
-            exclude = [user['chat_id']]
 
-        random_chat_id = users.get_random_user(exclude=exclude)
-        if not random_chat_id:
-            return False
+        partner_chat_id = chats.connect(user['chat_id'])
 
-        chats.connect(user['chat_id'], random_chat_id)
-        context.bot.send_message(
-            chat_id=random_chat_id,
-            text='К вам подключился новый собеседник',
-        )
-        return True
+        if partner_chat_id:
+            context.bot.send_message(
+                chat_id=partner_chat_id,
+                text='К вам подключился новый собеседник',
+            )
+        return bool(partner_chat_id)
--- mongo/chats.py
+++ mongo/chats.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from mongo.client import db
 
 
@@ -11,14 +13,36 @@ def get_all_chats():
     return db.chats.find({})
 
 
-def connect(first, second):
+def connect(chat_id: int) -> Optional[int]:
+    try:
+        busy_users = db.chats.aggregate([
+            {'$unwind': "$users"},
+            {'$group': {'_id': None, '_users': {'$addToSet': '$users'}}},
+            {'$project': {'_id': 0, 'users': '$_users'}}
+        ]).next()['users']
+    except (StopIteration, KeyError):
+        busy_users = []
+
+    try:
+        partner_chat_id = db.users.aggregate([
+            {'$match': {
+                'chat_id': {'$not': {'$in': busy_users}, '$ne': chat_id},
+                'paused': False,
+            }},
+            {'$sample': {'size': 1}}
+        ]).next()['chat_id']
+    except (StopIteration, KeyError):
+        return False
+
     db.chats.remove({
-        'users': {'$in': [first, second]},
+        'users': {'$in': [chat_id, partner_chat_id]},
     })
 
     db.chats.insert_one({
-        'users': [first, second],
+        'users': [chat_id, partner_chat_id],
     })
+
+    return partner_chat_id
 
 
 def disconnect(chat_id):
