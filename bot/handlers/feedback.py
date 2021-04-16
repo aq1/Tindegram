43a53f0073078ea48289c694661308111c545a80
Vladimir Grechukhin <inmosh@protomail.ch>
2021-04-16 22:31:02 +0300

    Change feedback command

--- bot/handlers/feedback.py
+++ bot/handlers/feedback.py
@@ -18,7 +18,7 @@ class Feedback(BaseCommand):
     )
 
     FAIL_TEXT = (
-        'Пишите сообщение сразу, например "/connect привет"'
+        'Пишите сообщение сразу, например "/feedback привет"'
     )
 
     def _execute(self, user: dict, update: Update, context: CallbackContext) -> bool:
@@ -28,7 +28,7 @@ class Feedback(BaseCommand):
         for admin in settings.TELEGRAM_ADMINS:
             context.bot.send_message(
                 chat_id=admin,
-                text='{}-{} написал\n{}'.format(
+                text='{} {}\n{}'.format(
                     users.get_user_str(user),
                     user['chat_id'],
                     ' '.join(context.args),
