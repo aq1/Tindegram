dd461192af35ab4dd750f52afe3200b0f0e631aa
Vladimir Grechukhin <inmosh@protomail.ch>
2021-04-16 13:36:37 +0300

    Change show_users.py

--- bot/handlers/admin/show_users.py
+++ bot/handlers/admin/show_users.py
@@ -9,9 +9,9 @@ from .base import BaseAdminCommand
 class ShowUsers(BaseAdminCommand):
     def _execute(self, user: dict, update: Update, context: CallbackContext) -> bool:
         _users = [
-            '{} - {}'.format(
+            '{}{}'.format(
                 users.get_user_str(u),
-                u['paused'],
+                ' - paused' if u['paused'] else '',
             )
             for u in users.get_all_users()
         ]
